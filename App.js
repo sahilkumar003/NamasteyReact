8b869f4472797cb255f0368f313aec99ec7a01fa
Sahil Kumar <117456815+sahilkumar003@users.noreply.github.com>
2023-09-17 14:33:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
+
 /**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
+ * 01. Add heading using Javascript
  */
+const jsHeading = document.createElement('h1');
+jsHeading.innerHTML = 'Hello World From Javascript!';
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+const jsRroot = document.getElementById('root');
+jsRroot.appendChild(jsHeading);
 
-//JSX
 
-console.log(parent); // object
+/**
+ * 02. Add heading using React
+ */
+const reactHeading = React.createElement('h1', {}, 'Hello World From React!');
+const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
+reactRoot.render(reactHeading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+/**
+ * Add nested HTML
+ */
+const nested = React.createElement('div', { id: 'parent' }, [
+	React.createElement('div', { id: 'child1' }, [
+		React.createElement('h1', {}, 'dddddHello, Iam H1 Tag Inside Child 1.'),
+		React.createElement('h2', {}, 'Hello, Iam H2 Tag.')
+	]),
+	React.createElement('div', { id: 'child2' }, [
+		React.createElement('h1', {}, 'Hello, Iam H1 Tag Child 2.'),
+		React.createElement('h2', {}, 'Hello, Iam H2 Tag Child 2.')
+	])
+])
+
+reactRoot.render(nested);
