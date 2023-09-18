b415a045e68c85c2416a57ace5acc8bac5882631
sahilkumar003 <kumarsahil944795@gmail.com>
2023-09-18 19:35:41 +0530

    Chapter 2

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+/**
+ * 01. Add heading using Javascript
+ */
+const jsHeading = document.createElement("h1");
+jsHeading.innerHTML = "Hello World From Javascript!";
+
+const jsRroot = document.getElementById("root");
+jsRroot.appendChild(jsHeading);
+
+/**
+ * 02. Add heading using React
+ */
+const reactHeading = React.createElement("h1", {}, "Hello World From React!");
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(reactHeading);
+
+/**
+ * Add nested HTML
+ */
+const nested = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "dddddHello, Iam H1 Tag Inside Child 1."),
+    React.createElement("h2", {}, "Hello, Iam H2 Tag."),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "Hello, Iam H1 Tag Child 2."),
+    React.createElement("h2", {}, "Hello, Iam H2 Tag Child 2."),
   ]),
 ]);
 
-//JSX
-
-console.log(parent); // object
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+reactRoot.render(nested);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React 🚀
+
+# Parcel
+
+- Dev build
+- Local server
+- Hot module replacement
+- File watching algorithm written in c++
+- Caching - faster builds
+- Image optimization
+- Minification
+- Building
+- Compress files
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - to support older browsers
+- Diagnostic
+- Error handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod build
