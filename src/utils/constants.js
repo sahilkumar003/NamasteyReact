3000da14b676fa276b06ce9370368a4141141797
sahilkumar003 <kumarsahil944795@gmail.com>
2023-11-10 08:56:31 +0530

    Restructure the app and introduce the hooks

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const LOGO_URL =
+  "https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg";
+
+export const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
